179d59d6ad3116a41a2573ff4984870883738e0d
Kyle Doherty <kyledoherty22@gmail.com>
2014-04-27 20:23:57 -0700

    commented out the .halt methods to disable the login functionality

--- dog.rb
+++ dog.rb
@@ -23,13 +23,13 @@ get '/dogs' do
 end
 
 get '/dogs/new' do 
-	halt(401, 'Not Authorized') unless session[:admin]
+	#halt(401, 'Not Authorized') unless session[:admin]
 	@dog = Dog.new
 	slim :new_dog
 end 
 
 post '/dogs' do 
-	halt(401, 'Not Authorized') unless session[:admin]
+	#halt(401, 'Not Authorized') unless session[:admin]
 	dog = Dog.create(params[:dog])
 	redirect to("/dogs/#{dog.id}")
 end 
@@ -40,7 +40,7 @@ get '/dogs/:id' do
 end 
 
 get '/dogs/:id/edit' do
-	halt(401, 'Not Authorized') unless session[:admin]
+	#halt(401, 'Not Authorized') unless session[:admin]
 	@dog = Dog.get(params[:id])
 	slim :edit_dog
 end 
@@ -52,7 +52,7 @@ put '/dogs/:id' do
 end 
 
 delete '/dogs/:id' do 
-	halt(401, 'Not Authorized') unless session[:admin]
+	#halt(401, 'Not Authorized') unless session[:admin]
 	Dog.get(params[:id]).destroy
 	redirect to('/dogs')
 end 
